ce601719876239287b144f7ec49204e756c1a0e2
juerg-mueller <j.mueller5524@gmail.com>
2024-09-20 15:57:53 +0200

    Add files via upload

---
+++ MidiRekorder.dpr
@@ -0,0 +1,36 @@
+﻿program MidiRekorder;
+// Für MAC
+//
+// brew install rtmidi
+// brew install lazarus
+// Für Linux
+//
+// sudo apt install librtmidi-dev
+// sudo apt install fluidsynth
+uses
+  {$ifdef FPC}
+  urtmidi,
+  {$else}
+  Midi in 'Midi.pas',
+  {$endif }
+  Forms,
+  {$ifdef FPC}
+  lcl, Interfaces,
+  {$endif }
+  UMidirekorder in 'umidirekorder.pas' {MidiGriff},
+  UMidiSaveStream in 'UMidiSaveStream.pas',
+  UMidiEvent in 'UMidiEvent.pas',
+  UMyMemoryStream in 'UMyMemoryStream.pas',
+  UMyMidiStream in 'UMyMidiStream.pas',
+  UMidiDataStream in 'UMidiDataStream.pas',
+  UEventArray in 'UEventArray.pas',
+  UBanks;
+
+begin
+{$ifdef FPC}
+  Application.Scaled:=True;
+{$endif}
+  Application.Initialize;
+  Application.CreateForm(TMidiGriff, MidiGriff);
+  Application.Run;
+end.
